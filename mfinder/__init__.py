9598ce7aac05a5dbfd92b5e9857be3ac0451205c
cboxtvbot <cboxtvbot@gmail.com>
2025-05-15 12:02:30 +0000

    Update __init__.py

--- mfinder/__init__.py
+++ mfinder/__init__.py
@@ -11,18 +11,18 @@ load_dotenv()
 id_pattern = re.compile(r"^.\d+$")
 
 # vars
-APP_ID = os.environ.get("APP_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+APP_ID = os.environ.get("APP_ID", "28862799")
+API_HASH = os.environ.get("API_HASH", "3dc3be3bfb934a68aa280044b064ccee")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8074681333:AAHNHcNbU0nr3epf0fti_AHH36nfzSXxSqU")
+DB_URL = os.environ.get("DB_URL", " mongodb+srv://cboxtvbot:Qwerty123%@cluster0.pjhktae.mongodb.net/")
+OWNER_ID = int(os.environ.get("OWNER_ID", "7886433884"))
 ADMINS = [
     int(user) if id_pattern.search(user) else user
-    for user in os.environ.get("ADMINS", "").split()
+    for user in os.environ.get("ADMINS", "7886433884").split()
 ] + [OWNER_ID]
 DB_CHANNELS = [
     int(ch) if id_pattern.search(ch) else ch
-    for ch in os.environ.get("DB_CHANNELS", "").split()
+    for ch in os.environ.get("DB_CHANNELS", "-1002543001708").split()
 ]
 
 try:
@@ -30,10 +30,10 @@ try:
 except Exception:
     import sample_const as const
 
-START_MSG = const.START_MSG
-START_KB = const.START_KB
-HELP_MSG = const.HELP_MSG
-HELP_KB = const.HELP_KB
+START_MSG = const. welcome
+START_KB = const.welcome
+HELP_MSG = const.welcome
+HELP_KB = const.welcome
 
 
 # logging Conf
